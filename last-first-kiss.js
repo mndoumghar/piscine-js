de636321ae0b1100e7c07a367f58fcbbc78b7ae4
mndoumghar <mohamedndoumghar@gmail.com>
2025-06-19 17:07:33 +0100

    return last and first elements

---
+++ last-first-kiss.js
@@ -0,0 +1,16 @@
+function first(arr) {
+    return arr[0]
+
+}
+
+function last(arr) {
+    return arr[(arr.length) - 1]
+}
+
+function kiss(arr) {
+
+    return [arr[0], arr[(arr.length) - 1]]
+
+
+}
+
--- more-or-less.js
+++ more-or-less.js
@@ -3,6 +3,7 @@ const more = (b) => b+1
 const less= (x) => x-1
 
  function add(x, y) {
+    
     return x+y
  }
 
