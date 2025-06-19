cb9bf0de9052beaea0e0498f3c370ada9df311ca
mndoumghar <mohamedndoumghar@gmail.com>
2025-06-19 13:14:04 +0100

    Decalaration array anbd object

--- declarations.js
+++ declarations.js
@@ -9,3 +9,16 @@ obj = {
     bool: true,
     undef: undefined
 }
+nested = {
+    arr: [4, undefined, '2'],
+    obj: {
+        str: "hello ",
+        num: 1,
+        bool: true,
+    }
+}
+Object.freeze(nested)
+Object.freeze(obj)
+
+
+
