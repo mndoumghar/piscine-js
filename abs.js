f6869f0f2eb2a93cc39ae0aa4c2a52711b52e036
mndoumghar <mohamedndoumghar@gmail.com>
2025-06-20 12:12:01 +0100

    Math Abc

--- abs.js
+++ abs.js
@@ -1,2 +1,5 @@
-const isPositive = (x) => x > 0 ? true : false
-const abs = (x) => x > 0 ? x : Math.abs(x) 
+const isPositive  = (x) => x > 0 ? true : false
+const abs = (x) => x > 0 ? Math.abs(x) : Math.abs(x) 
+
+console.log(isPositive(-12))
+console.log(abs(0))
