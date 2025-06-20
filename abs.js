831dfd9d11209e0c6d19fab9777fd15420e07436
mndoumghar <mohamedndoumghar@gmail.com>
2025-06-20 12:46:29 +0100

    Sign

--- abs.js
+++ abs.js
@@ -1,5 +1,7 @@
 const isPositive  = (x) => x > 0 ? true : false
 const abs = (x) => isPositive(x) || x== 0 ? x  : x * -1 
-
 console.log(isPositive(0))
 console.log(abs(0))
+
+
+
---
+++ sign.js
@@ -0,0 +1,3 @@
+const isPositive = (x) => x > 0 ? true : false
+const sign = (x) => isPositive(x) ? 1 : x == 0 ? 0 : -1
+
