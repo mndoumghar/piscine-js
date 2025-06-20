94039687311529d5f38e8b80ca846e67e08e6f58
mndoumghar <mohamedndoumghar@gmail.com>
2025-06-20 12:19:01 +0100

    ABS

--- abs.js
+++ abs.js
@@ -1,5 +1,5 @@
-const isPositive  = (x) => x > 0 ? true : false
-const abs = (x) => x > 0 ? Math.abs(x) : Math.abs(x) 
+const isPositive  = (x) => x >= 0 ? true : false
+const abs = (x) => isPositive(x) ? x  : x * -1 
 
 console.log(isPositive(-12))
 console.log(abs(0))
--- method-man.js
+++ method-man.js
@@ -22,7 +22,7 @@ function capitalize(x) {
     return x.charAt(0).toUpperCase() + x.slice(1).toLowerCase();
 }
 
-console.log(words('hello hfgH'))
-console.log(sentence(['hello', 'hfgH']))
-console.log(whisper('DÉJÀ VU'))
-console.log(capitalize('STR c bv'))
+// console.log(words('hello hfgH'))
+// console.log(sentence(['hello', 'hfgH']))
+// console.log(whisper('DÉJÀ VU'))
+// console.log(capitalize('STR c bv'))
