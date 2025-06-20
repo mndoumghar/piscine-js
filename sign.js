76e5954bca066517b3699d8a927d29e27fb38674
mndoumghar <mohamedndoumghar@gmail.com>
2025-06-20 13:00:01 +0100

    Sign

--- sign.js
+++ sign.js
@@ -1,2 +1,6 @@
-const sign = (x) => x>0 ? 1 : x == 0 ? 0 : -1
+const isPositive = (x) => x > 0 ? true : false
+const sign = (x) => isPositive(x) ? 1 : (x < 0) ? -1 : 0
 
+const sameSign  = (x,y) => (sign(x)  == sign(y)) ? true : false
+console.log(sign(414))
+console.log(sameSign(-4,-121))
