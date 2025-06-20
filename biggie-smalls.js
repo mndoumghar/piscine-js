2e5f5942149490e2cc9252081d39300baf7e83ce
mndoumghar <mohamedndoumghar@gmail.com>
2025-06-20 11:05:06 +0100

    biggie smales

--- biggie-smalls.js
+++ biggie-smalls.js
@@ -1,5 +1,5 @@
-const smalls = Number.MIN_VALUE
-const biggie = Number.MAX_VALUE
+const smalls = Number.NEGATIVE_INFINITY
+const biggie = Number.POSITIVE_INFINITY
 console.log(smalls)
 
 console.log(biggie)
