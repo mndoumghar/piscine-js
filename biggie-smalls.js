c1ceb207602cf1c3e11f1f42560dd037e686f76b
mndoumghar <mohamedndoumghar>
2025-06-20 10:55:12 +0100

    Take number

--- biggie-smalls.js
+++ biggie-smalls.js
@@ -1,5 +1,7 @@
-smalls = Number.MIN_SAFE_INTEGER
-biggie = Number.MAX_SAFE_INTEGER
+const smalls = Number.MIN_SAFE_INTEGER
+const biggie = Number.MAX_SAFE_INTEGER
+console.log(smalls)
 
+console.log(biggie)
 
 
