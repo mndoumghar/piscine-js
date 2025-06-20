92d3394505c9bd328fe3294536ecfe25a70f1910
mndoumghar <mohamedndoumghar@gmail.com>
2025-06-20 16:59:38 +0100

    Physics.js [200~Isaac~

---
+++ physics.js
@@ -0,0 +1,32 @@
+
+/*
+
+const getAcceleration = (obj) => {
+  if (obj.f != undefined && obj.m != undefined) {
+    return obj.f / obj.m;
+  } else if (obj.Δv != undefined && obj.Δt != undefined) {
+    return obj.Δv / obj.Δt;
+  } else if (obj.t != undefined && obj.d != undefined) {
+    return 2*obj.d/Math.pow(obj.t,2)
+  } else {
+    return "impossible";
+  }
+};
+*/
+
+const getAcceleration = (Isaac) =>
+
+    Isaac.f != undefined && Isaac.m != undefined
+
+        ? Isaac.f / Isaac.m
+
+
+        : Isaac.Δv != undefined && Isaac.Δt != undefined
+            ? Isaac.Δv / Isaac.Δt 
+
+            : Isaac.d != undefined && Isaac.t!= undefined
+
+                ? (2 * Isaac.d) / (Isaac.t * Isaac.t)
+
+                    : "impossible"
+
