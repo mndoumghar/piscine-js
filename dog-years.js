ec6c0871ce3e1eab598351e1d74b71fd50e2af08
mndoumghar <mohamedndoumghar>
2025-06-20 10:01:19 +0100

    DogYears

--- dog-years.js
+++ dog-years.js
@@ -9,22 +9,24 @@ function dogYears(name, age) {
             return Number(((age * 7 ) / (0.2408467)/life).toFixed(2)) 
         }
     if (name == "venus") {
-            return Number(((age * 7) / (0.61519726 * 24 * 60 * 60)/life).toFixed(2))
+            return Number(((age * 7) / (0.61519726)/life).toFixed(2))
         }
     if (name == "mars") {
-            return Number(((age * 7) / (1.8808158 * 24 * 60 * 60)/life).toFixed(2))
+            return Number(((age * 7) / (1.8808158)/life).toFixed(2))
         }
     if (name == "jupiter") {
-            return Number(((age * 7) / (11.862615 * 24 * 60 * 60)/life).toFixed(2))
+            return Number(((age * 7) / (11.862615)/life).toFixed(2))
         }
     if (name == "saturn") {
-            return Number(((age * 7) / (29.447498 * 24 * 60 * 60)/life).toFixed(2))
+            return Number(((age * 7) / (29.447498)/life).toFixed(2))
         }
     if (name == "uranus") {
-            return Number(((age * 7) / (84.016846 * 24 * 60 * 60)/life).toFixed(2))
+            return Number(((age * 7) / (84.016846)/life).toFixed(2))
         }
     if (name == "neptune") {
-            return Number(((age * 7) / (164.79132 * 24 * 60 * 60)/life).toFixed(2))
+            return Number(((age * 7) / (164.79132)/life).toFixed(2))
         }
 }
 console.log(dogYears('mercury', 2134835688))
+
+console.log(dogYears('venus', 189839836))
