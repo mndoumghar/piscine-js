371229ac853c4cabc3083f42d49a6e811e5440b4
mndoumghar <mohamedndoumghar@gmail.com>
2025-06-20 15:57:20 +0100

    is ...

--- is.js
+++ is.js
@@ -1,119 +1,108 @@
 
-const is = {
-
-
-
-
-    num: function (num) {
+is.num =  function (num) {
 
         if ((typeof num) == "number") {
             return true
         } else {
             return false
         }
-    },
+    }
 
 
-    nan: function (nan) {
+   is.nan =  function (nan) {
 
         if ((typeof nan) == "NaN") {
             return true
         } else {
             return false
         }
-    },
+    }
 
-    str: function (str) {
+    is.str=  function (str) {
 
         if ((typeof str) == "string") {
             return true
         } else {
             return false
         }
-    },
+    }
 
-    bool: function (bool) {
+    is.bool =  function (bool) {
 
         if ((typeof bool) == "boolean") {
             return true
         } else {
             return false
         }
-    },
+    }
 
-    undef: function (undef) {
+    is.undef =  function (undef) {
 
         if ((typeof undef) == "undefined") {
             return true
         } else {
             return false
         }
-    },
+    }
 
-    def: function (def) {
+    is.def=  function (def) {
 
         if ((typeof def) == "NaN") {
             return true
         } else {
             return false
         }
-    },
+    }
 
-    arr: function (arr) {
+    is.arr=  function (arr) {
 
         if ((typeof num) == "array") {
             return true
         } else {
             return false
         }
-    },
+    }
 
-    obj: function (obj) {
+    is.obj =  function (obj) {
 
         if ((typeof obj) == "object") {
             return true
         } else {
             return false
         }
-    },
+    }
 
 
-     fun: function (fun) {
+     is.fun =  function (fun) {
 
         if ((typeof fun) == "function") {
             return true
         } else {
             return false
         }
-    },
+    }
 
-     truthy: function (truthy) {
+     is.truthy=  function (truthy) {
 
         if ((typeof truthy) == "truthy") {
             return true
         } else {
             return false
         }
-    },
+    }
 
 
-     falsy: function (falsy) {
+     is.falsy =  function (falsy) {
 
         if ((typeof falsy) == "falsy") {
             return true
         } else {
             return false
         }
-    },
+    }
 
 
 
 
-
-
-
-
-
-}
 
 
