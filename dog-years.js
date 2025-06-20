8478e52886b450e4f21ff2dfb33d456e92ba990f
mndoumghar <mohamedndoumghar>
2025-06-20 09:37:43 +0100

    DogYears

--- dog-years.js
+++ dog-years.js
@@ -1,6 +1,7 @@
 
 
 function dogYears(name, age) {
+
     if (name == "earth") {      
     
         return ((age * 7) / (365.25 * 24 * 60 * 60)).toFixed(2)
@@ -8,25 +9,26 @@ function dogYears(name, age) {
     }
 
         if (name == "mercury") {
-            return ((age * 7) / (0.2408467 * 24 * 60 * 60)).toFixed(2)
+
+            return Number(((age * 7) / (0.2408467 * 24 * 60 * 60)).toFixed(2)) 
         }
     if (name == "venus") {
-            return ((age * 7) / (0.61519726 * 24 * 60 * 60)).toFixed(2)
+            return Number(((age * 7) / (0.61519726 * 24 * 60 * 60)).toFixed(2))
         }
     if (name == "mars") {
-            return ((age * 7) / (1.8808158 * 24 * 60 * 60)).toFixed(2)
+            return Number(((age * 7) / (1.8808158 * 24 * 60 * 60)).toFixed(2))
         }
     if (name == "jupiter") {
-            return ((age * 7) / (11.862615 * 24 * 60 * 60)).toFixed(2)
+            return Number(((age * 7) / (11.862615 * 24 * 60 * 60)).toFixed(2))
         }
     if (name == "saturn") {
-            return ((age * 7) / (29.447498 * 24 * 60 * 60)).toFixed(2)
+            return Number(((age * 7) / (29.447498 * 24 * 60 * 60)).toFixed(2))
         }
     if (name == "uranus") {
-            return ((age * 7) / (84.016846 * 24 * 60 * 60)).toFixed(2)
+            return Number(((age * 7) / (84.016846 * 24 * 60 * 60)).toFixed(2))
         }
     if (name == "neptune") {
-            return ((age * 7) / (164.79132 * 24 * 60 * 60)).toFixed(2)
+            return Number(((age * 7) / (164.79132 * 24 * 60 * 60)).toFixed(2))
         }
 
 
