5655da8c5d0b4c8cf24f43f19de81a27bcb2cb07
mndoumghar <mohamedndoumghar@gmail.com>
2025-06-21 11:28:55 +0100

    elemnets

--- elementary.js
+++ elementary.js
@@ -1,18 +1,18 @@
 const multiply = (x, y) => {
     var i = 0
     var sign = ''
-    if(x<0 || y<0) {
-    
-        sign = '-'  
+    if (x < 0 || y < 0) {
+
+        sign = '-'
     }
-    if(x<0 && y<0) {
-    
-        sign = ''  
+    if (x < 0 && y < 0) {
+
+        sign = ''
     }
-    x=  Math.abs(x)
+    x = Math.abs(x)
     y = Math.abs(y)
 
-    
+
 
     var stock = 0
     while (i < y) {
@@ -20,11 +20,10 @@ const multiply = (x, y) => {
         stock += x
         i++
     }
-    return Number(sign+stock)
+    return Number(sign + stock)
 }
 const divide = (x, y) => {
 
-    var res = 0
     if (x == y) {
         return 1
     }
@@ -38,33 +37,45 @@ const divide = (x, y) => {
     if (y == 0 || x == 0) {
         return 0
     }
-        var i = 0
+    var i = 0
 
-    while (i < x) {
+
+    var res
+
+    while (i <= x) {
+
 
         x = x - y
+        res = x
+        if (res < 0) {
+            break
+        }
+
+
         i++
+
     }
     return i
 }
+// console.log(divide(78, 34))
+
 function modulo(x, y) {
 
-    if (x === 0 ) {
-        return 0 
+    if (x === 0) {
+        return 0
     }
 
-    if (x>0  && y > 0) {
-    var counter = x
+    if (x > 0 && y > 0) {
+        var counter = x
 
-    while (counter>  y) {
+        while (counter > y) {
 
-        counter -= y
-    }
-    return counter
+            counter -= y
+        }
+        return counter
     }
 }
 
-console.log(multiply(123, -22))
-console.log(multiply(-22, -123) )
-console.log(divide(8, 4))
-console.log(modulo(8,2))
+// console.log(multiply(123, -22))
+// console.log(multiply(-22, -123))
+// console.log(modulo(8, 2))
