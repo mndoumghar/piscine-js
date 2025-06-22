1276703421248441b8077da876b0fe8bcabe2051
mndoumghar <mohamedndoumghar@gmail.com>
2025-06-22 16:15:01 +0100

    Chunk function

--- chunky.js
+++ chunky.js
@@ -1,38 +1,3 @@
-function slice(arr1, start, end) {
-    // Declatatoin Tabl array 
-    let arr = []
-    if (start < 0) {
-
-        start += arr1.length
-    }
-
-    if (end < 0) {
-        end += arr1.length
-    }
-
-
-    if (end == undefined) {
-
-        for (let i = start; i < arr1.length; i++) {
-            // ADD a new elemnts IN array 
-            arr.push(arr1[i])
-        }
-
-    } else {
-
-        for (let i = start; i < end; i++) {
-            arr.push(arr1[i])
-        }
-    }
-
-    if (typeof arr1 == "string") {
-        return arr.join('')
-    }
-    return arr
-
-}
-
-
 
 
 function chunk(arr, step) {
@@ -40,10 +5,13 @@ function chunk(arr, step) {
     let i = 0
 
     while (i < arr.length) {
-        let part = slice(arr, i, i + step)
-       // console.log(part,i)
-        result.push(part)
+        
+        let part = arr.slice(i, i + step)
+        if ( typeof part != undefined) {
+            result.push(part)
         i += step
+        }
+        
     }
 
     return result
@@ -53,4 +21,4 @@ function chunk(arr, step) {
 
 const animals = ["ant", "bison", "camel", "duck", "elephant"];
 
-console.log(chunk(animals, 3))
+console.log(chunk(['a', 'b', 'c', 'd'], 3))
