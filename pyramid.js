5df72bbecc26c08f53ddfc7b88b78f06bcffa76d
mndoumghar <mohamedndoumghar@gmail.com>
2025-06-22 18:09:56 +0100

    pyramid

--- pyramid.js
+++ pyramid.js
@@ -3,11 +3,9 @@ function pyramid(x, y) {
     const symbolWidth = x.length;
 
     for (let i = 1; i <= y; ++i ) {
-        // Add appropriate number of spaces for padding
         for (let space = 1; space <= y - i; ++space) {
             res += " ".repeat(symbolWidth);
         }
-        // Add symbols
         for (let k = 0; k < 2 * i - 1; ++k) {
             res += x;
         }
