c24035059392b2aa96d9508dc55ac11731ea145e
mndoumghar <mohamedndoumghar@gmail.com>
2025-06-23 09:38:13 +0100

    letter space number

--- letter-space-number.js
+++ letter-space-number.js
@@ -1,13 +1,13 @@
 
 function letterSpaceNumber(str1) {
 
-    const res = /\w\s\d(?!\d)/g
-    return str1.match(res)
+    const res = /\w\s\d(?!\w)/g
+    return str1.match(res) || []
     
 
 }
 
 
-console.log(letterSpaceNumber('He is 8 or 9 years old, not 10.')) 
+console.log(letterSpaceNumber('I like 7up.')) 
 
 // output: ['e 1']
