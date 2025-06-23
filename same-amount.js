cb8866e21431ff81dc729f2708dbc5b03292602b
mndoumghar <mohamedndoumghar@gmail.com>
2025-06-23 12:11:43 +0100

    same amount

--- same-amount.js
+++ same-amount.js
@@ -1,9 +1,19 @@
-function sameAmount(str) {
-        return str
+function sameAmount(str, rg1, rg2) {
+    const r1 = new RegExp(rg1.source, 'g');
+    const r2 = new RegExp(rg2.source, 'g')
+
+
+    const match1 = str.match(r1) || []
+    const match2 = str.match(r2) || []
+    console.log(match1, match2)
+    
+
+    return match1.length=== match2.length  
 
 }
 
-const str = "table football"
+const str = "table fiootball"
 const regix = /fo+/
-const globalRegix = /fo+/g
-console.log(regix.test(str))
+const globalRegix = /fbo+/g
+
+console.log(sameAmount(str,regix, globalRegix ))
