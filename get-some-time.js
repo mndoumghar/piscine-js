8941a3d0bf41a19868ba76efb010d927403680f8
mndoumghar <mohamedndoumghar@gmail.com>
2025-06-26 19:02:21 +0100

    get-some time

--- get-some-time.js
+++ get-some-time.js
@@ -1,9 +1,18 @@
 const firstDayWeek = (week, years) => {
-    const date = new Date(years)  
-    const d = date.getDay() 
-     const addweek = (week - 1) * 7 - d
+    const date = new Date(years)
+    const d = date.getDay()
+    const addweek = (week - 1) * 7 - d
     const res = new Date(date.getTime() + addweek * 24 * 3600 * 1000)
-    return res
+    let mm = res.getMonth() + 1; 
+    let dd = res.getDate();
+    let yyyy = res.getFullYear()
+
+    if (dd < 10) dd = '0' + dd;
+    if (mm < 10) mm = '0' + mm;
+
+    const formattedres = dd + '-' + mm + '-' + yyyy;
+
+    return formattedres
 }
 
 
