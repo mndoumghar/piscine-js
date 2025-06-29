229ffdec6fed37b75c24a08727b3442ada15f591
mndoumghar <mohamedndoumghar@gmail.com>
2025-06-29 04:47:53 +0100

    has city

--- has-city.js
+++ has-city.js
@@ -4,7 +4,7 @@ function hasCity(country, cities) {
     return function (city) {
 
 
-        return cities.includes(city)
+        return cities.includes(city) // check if true find city from this country 
             ? `${city} is a city from ${country}`
             : `${city} is not a city from ${country}`
 
