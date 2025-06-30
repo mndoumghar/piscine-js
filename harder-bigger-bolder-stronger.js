b95928177f343e5af00af622c5c0b4f3a6fc535f
mndoumghar <mohamedndoumghar@gmail.com>
2025-06-30 16:09:55 +0100

    update

---
+++ harder-bigger-bolder-stronger.js
@@ -0,0 +1,25 @@
+
+
+
+ export const generateLetters = () => {
+const body = document.querySelector("body")
+const div = document.createElement('div')
+body.append(div)
+
+    for (let i = 1; i < 120; i++) {
+        const CreatDr = document.createElement('div')
+
+        for (let j = 1; j <= 10; j++) {
+            CreatDr.  = j
+        
+        }
+            CreatDr.style.fontSize = "11px"  
+
+                    body.append(CreatDr)
+
+
+    
+    }
+
+
+}
