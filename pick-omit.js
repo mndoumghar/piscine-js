988f3b1c454772377659b111f2336176b82adba1
mndoumghar <mohamedndoumghar@gmail.com>
2025-07-02 16:09:17 +0100

    pick omit

--- pick-omit.js
+++ pick-omit.js
@@ -1,14 +1,20 @@
 function pick(obj, arr) {
     for (const i of arr) {
         if (!obj.hasOwnProperty(i)) {
-            return false
+            return obj
         }
         
     }
-    return true
+    return obj
 }
 
 function omit(obj,key) {
     return true
 }
 
+const myObject = { a: 1, b: 2, c: 3 };
+const keyArray1 = ["a", "b", "c"];
+const keyArray2 = ["a", "b", "d"];
+
+console.log(pick(myObject, keyArray1));
+console.log(pick(myObject, keyArray2));
