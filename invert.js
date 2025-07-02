45ee378236e5232e060eb539f73c3ef0a66f3b95
mndoumghar <mohamedndoumghar@gmail.com>
2025-07-02 15:36:51 +0100

    invert

--- invert.js
+++ invert.js
@@ -2,8 +2,21 @@
 
 function invert(obj) {
 
-    return Object.keys(obj)
+    let key = Object.values(obj)
+    let value = Object.keys(obj)
+    let stock = {}
+    for (let i = 0; i < value.length; i++) {
+        stock[key[i]] = value[i]
 
+    }
     
+    return stock
+
+
+
 }
+
+
+
+console.log(invert({ language: 'english' }));
 
