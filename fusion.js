001f0e18d338727dbfe9e0a1d023f61d6d73bfc2
mndoumghar <mohmamedndoumghar@gmail.com>
2025-07-03 21:14:36 +0100

    string ... remove empty ' '

--- fusion.js
+++ fusion.js
@@ -39,36 +39,3 @@ function fusion(object1, object2) {
 
     return res
 }
-
-//fusion({ a: 10, b: 8, c: 1 }, { a: 10, b: 2 }); // -> { a: 20, b: 10, c: 1 }
-
-console.log(fusion({ a: 10, b: 8, c: 1 }, { a: 10, b: 2 }));
-
-
-console.log(fusion({ arr: [1, "2"] }, { arr: [2] }));
-
-//fusion({ arr: [], arr1: [5] },{ arr: [10, 3], arr1: [15, 3], arr2: ["7", "1"] }); // ->{ arr: [ 10, 3 ], arr1: [ 5, 15, 3 ], arr2: [ '7', '1' ] }
-
-
-
-console.log(fusion({ arr: [], arr1: [5] }, { arr: [10, 3], arr1: [15, 3], arr2: ["7", "1"] }));
-
-//fusion({ str: "salem" }, { str: "alem" }); // -> { str: 'salem alem' }
-console.log(fusion({ str: "salem" }, { str: "alem" }));
-//fusion({ str: "salem" }, { str: "" }); // -> { str: 'salem ' }
-
-console.log(fusion({ str: "salem" }, { str: "" }));
-
-///fusion({ a: 1, b: { c: "Salem" } }, { a: 10, x: [], b: { c: "alem" } }); // -> { a: 11, x: [], b: { c: 'Salem alem' } }
-
-//console.log(fusion({ a: 1, b: { c: "Salem" } }, { a: 10, x: [], b: { c: "alem" } }))
-
-
-
-//fusion( { a: { b: [3, 2], c: { d: 8 } } },{ a: { b: [0, 3, 1], c: { d: 3 } } }); // -> { a: { b: [ 3, 2, 0, 3, 1 ], c: { d: 11 } } }
-console.log(fusion( { a: { b: [3, 2], c: { d: 8 } } },{ a: { b: [0, 3, 1], c: { d: 3 } } }));
-//  eq(fusion({ str: 'hello' }, { str: 'there' }).str, 'hello there')
-console.log(fusion({ str: 'hello' }, { str: 'there' }));
-
-
-
