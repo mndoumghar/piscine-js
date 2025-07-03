f89a318c402961fea393c132074f6babe7216acc
mndoumghar <mohmamedndoumghar@gmail.com>
2025-07-03 21:10:50 +0100

    fusion

--- fusion.js
+++ fusion.js
@@ -1,40 +1,44 @@
+function fusion(object1, object2) {
+     
+    let array = [object1, object2]
+    const res = {}
 
-// fusion({ arr: [1, "2"] }, { arr: [2] }); // -> { arr: [ 1, '2', 2 ] }
+    array.forEach(obj => {
 
-function fusion(obj1, obj2) {
-    
-    
- const result = {};
- const key1 =  Object.keys(obj1)
-  const key2 =  Object.keys(obj2)
- 
-    for (const key in obj1) {
-         result[key] = obj2[(key)]
-    }
-    const res = Object.assign({}, result)
+        Object.entries(obj).forEach(([key, value]) => {
 
+            if (res[key] !== undefined) {
 
-    return  res
+                if (typeof value === 'object' &&!Array.isArray(value) && typeof res[key] === 'object' && !Array.isArray(res[key])) {
+
+                         res[key] = fusion(res[key], value)
+
+                } else if (Array.isArray(res[key]) && Array.isArray(value)) {
+
+                    res[key] = res[key].concat(value)
+
+                } else if (typeof res[key] === 'string' && typeof value === 'string') {
+                    
+                    res[key] = res[key] + value + ' '
+
+                } else if (typeof res[key] === 'number' && typeof value === 'number') {
+
+                    res[key] = res[key] + value
+                } else {
+
+                    res[key] = value
+                }
+            } else {
+                if (typeof value == "string") {
+
+                    res[key] = value + " "
+
+                } else {
+                    res[key] = value
+                }
+            }
+        })
+    })
+
+    return res
 }
-console.log(fusion({ arr: [1, "2"] }, { arr: [2] }))
-
-// Create a function named fusion that:
-
-// For array types, you will concatenate them.
-
-// fusion({ arr: [1, "2"] }, { arr: [2] }); // -> { arr: [ 1, '2', 2 ] }
-// fusion({ arr: [], arr1: [5] },{ arr: [10, 3], arr1: [15, 3], arr2: ["7", "1"] }); // ->{ arr: [ 10, 3 ], arr1: [ 5, 15, 3 ], arr2: [ '7', '1' ] }
-// For strings, you must concatenate them with a space.
-
-// fusion({ str: "salem" }, { str: "alem" }); // -> { str: 'salem alem' }
-// fusion({ str: "salem" }, { str: "" }); // -> { str: 'salem ' }
-// If they are numbers, you must add them.
-
-// fusion({ a: 10, b: 8, c: 1 }, { a: 10, b: 2 }); // -> { a: 20, b: 10, c: 1 }
-// If it is an object, you must join them recursively.
-
-// fusion({ a: 1, b: { c: "Salem" } }, { a: 10, x: [], b: { c: "alem" } }); // -> { a: 11, x: [], b: { c: 'Salem alem' } }
-// fusion( { a: { b: [3, 2], c: { d: 8 } } },{ a: { b: [0, 3, 1], c: { d: 3 } } }); // -> { a: { b: [ 3, 2, 0, 3, 1 ], c: { d: 11 } } }
-// In case of type mismatch you must replace it with the value of the second object (if it exists).
-
-// fusion({ a: "hello", b: [] }, { a: 4 }); // -> { a: 4, b: [] }
