bc9357fb033c79cc442df988bf361852b2ed64a9
mndoumghar <mohmamedndoumghar@gmail.com>
2025-07-04 02:24:23 +0100

    mzinpulate keys

--- manipulate-keys.js
+++ manipulate-keys.js
@@ -1,41 +1,28 @@
 const nutrients = { carbohydrates: 12, protein: 20, fat: 5 }
 
-
 function filterKeys(obj, funn) {
     let ff = Object.fromEntries(Object.entries(obj).filter(([key, value]) => funn(key)))
     return ff
 }
 
 console.log(filterKeys(nutrients, (key) => /protein/.test(key)))
-// output: { protein: 20 }
-
-
-
 
 function mapKeys(obj, gg) {
-
     const ff = Object.entries(obj).map(([key, value]) => [gg(key), value])
-
     return Object.fromEntries(ff)
 }
 
-
- console.log(mapKeys(nutrients, (k) => `-${k}`))
-// // output: { -carbohydrates: 12, -protein: 20, -fat: 5 }
+console.log(mapKeys(nutrients, (k) => `-${k}`))
 
 
-
-
-
-function reduceKeys(obj, gg) {
+function reduceKeys(obj, gg, initialValue) {
 
     const res = Object.keys(obj)
-
-    return res.reduce(gg)
-
+    
+    return res.reduce(gg, initialValue)
 }
 
 
+console.log(reduceKeys(nutrients, (acc, cr) => acc ? acc + ', ' + cr : cr, ''))
 
-console.log(reduceKeys(nutrients, (acc, cr) =>acc.concat(', ', cr)))
-// // output: carbohydrates, protein, fat
+console.log(reduceKeys(nutrients, (acc, cr) => `${acc}${cr}:`, ':'))
