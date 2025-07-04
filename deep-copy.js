21fb470f5ad4dd8af257b38e20b38326b87f84db
mndoumghar <mohmamedndoumghar@gmail.com>
2025-07-04 11:02:17 +0100

    deep copy

--- deep-copy.js
+++ deep-copy.js
@@ -1,26 +1,21 @@
 
 function deepCopy(obj) {
-
     if (typeof obj !== "object") {
         return obj
     }
-    
     let arr = []
     if (Array.isArray(obj)) {
         for (let i = 0; i < obj.length; i++) {
             arr[i] = deepCopy(obj[i])
         }
         return arr
-
     }
 
-
+    const copy = {};
+  for (const key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      copy[key] = deepCopy(obj[key])
+    }
+  }
+  return copy;
 }
-console.log(
-
-    deepCopy({ a: [12, 54] })
-
-
-);
-
-
