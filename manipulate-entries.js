f57e04c9eb02f1505c201f51fd7b202f87f4b638
mndoumghar <mohmamedndoumghar@gmail.com>
2025-07-04 04:20:59 +0100

    manipulate

--- manipulate-entries.js
+++ manipulate-entries.js
@@ -1,8 +1,8 @@
 
 function filterEntries(obj, ff) {
-  const entries = Object.entries(obj);
-  const filteredEntries = entries.filter(entry => ff(entry));
-  return Object.fromEntries(filteredEntries);
+  const entries = Object.entries(obj)
+  const filteredEntries = entries.filter(entry => ff(entry))
+  return Object.fromEntries(filteredEntries)
 }
 
 
@@ -14,27 +14,27 @@ function mapEntries(obj, ff) {
 
 
 function reduceEntries(obj, ff, i) {
-  const entries = Object.entries(obj);
+  const entries = Object.entries(obj)
   return entries.reduce(
     (accumulator, entry) => ff(accumulator, entry),
     i
-  );
+  )
 }
-
 function totalCalories(cart) {
-  return reduceEntries(cart, (acc, [item, qty]) => {
-    const calPer100 = nutritionDB[item]?.calories || 0;
-    return acc + (qty / 100) * calPer100;
-  }, 0);
+  const total = reduceEntries(cart, (acc, [item, qty]) => {
+    const calPer100 = nutritionDB[item]?.calories || 0
+    return acc + (qty / 100) * calPer100
+  }, 0)
+  return Math.round(total * 10) / 10 
 }
 
 
 function lowCarbs(cart) {
   return filterEntries(cart, ([item, qty]) => {
-    const carbsPer100 = nutritionDB[item]?.carbs || 0;
-    const totalCarbs = (qty / 100) * carbsPer100;
-    return totalCarbs < 50;
-  });
+    const carbsPer100 = nutritionDB[item]?.carbs || 0
+    const totalCarbs = (qty / 100) * carbsPer100
+    return  Math.round(totalCarbs < 50)
+  })
 }
 
 
@@ -42,13 +42,13 @@ function lowCarbs(cart) {
 
 function cartTotal(cart) {
   return mapEntries(cart, ([item, qty]) => {
-    const nutrition = nutritionDB[item] || {};
-    const totalNutrition = {};
+    const nutrition = nutritionDB[item] || {}
+    const totalNutrition = {}
 
     for (const [key, value] of Object.entries(nutrition)) {
-      totalNutrition[key] = (qty / 100) * value;
+      totalNutrition[key] = (qty / 100) * value
     }
 
-    return [item, totalNutrition];
-  });
+    return [item, totalNutrition]
+  })
 }
