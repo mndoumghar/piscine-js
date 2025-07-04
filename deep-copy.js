e0340ae96ac88cd51c11c63422c8e8c7a655a583
mndoumghar <mohmamedndoumghar@gmail.com>
2025-07-04 10:46:42 +0100

    deep copy

--- deep-copy.js
+++ deep-copy.js
@@ -1,20 +1,20 @@
-let res = []
 
-function deepCopy(d, point) {
+function deepCopy(obj) {
 
-    if (d == 0) {
-        res.push(point)
-    } else {
-        ex1 = [...point]
-        ex1.push(0)
 
-        ex2 = [...point]
-        ex2.push(1)
+return structuredClone(deepCopy(obj))
 
-        deepCopy(d - 1, ex1)
-        deepCopy(d - 1, ex2)
 
-    }
-
+    // let res = []
+    // if (d == 0) {
+    //     res.push(point)
+    // } else {
+    //     ex1 = [...point]
+    //     ex1.push(0)
+    //     ex2 = [...point]
+    //     ex2.push(1)
+    //     deepCopy(d - 1, ex1)
+    //     deepCopy(d - 1, ex2)
+    // }
 }
 
