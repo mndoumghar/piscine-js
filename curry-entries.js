1933fe68833e05d6ee7478187b5b5652b6ceaaaf
mndoumghar <mohmamedndoumghar@gmail.com>
2025-07-04 09:37:46 +0100

    curry entries

--- curry-entries.js
+++ curry-entries.js
@@ -1,11 +1,10 @@
 function defaultCurry(obj1) {
-
     return function name(obj2) {
-
         return Object.assign({}, obj1, obj2)
     }
 
 }
+
 
 function mapCurry(gg) {
     return function (obj) {
@@ -13,6 +12,8 @@ function mapCurry(gg) {
         return Object.fromEntries(ff);
     };
 }
+
+
 function reduceCurry(gg) {
   return function (obj, i = '') {
     const res = Object.entries(obj);
@@ -22,8 +23,8 @@ function reduceCurry(gg) {
   };
 }
 
-function filterCurry(funn) {
 
+function filterCurry(funn) {
     return function (obj) {
         const res = Object.fromEntries(Object.entries(obj).filter((key) => funn(key)))
         return res
@@ -31,24 +32,77 @@ function filterCurry(funn) {
     }
 
 }
+console.log(
 
-
-// filterCurry(([k, v]) => typeof v === 'string' || k === 'arr')({
-//     str: 'string',
-//     nbr: 1,
-// filterCurry(([k, v]) => typeof v === 'string' || k === 'arr')({
-//     str: 'string',
-//     nbr: 1,
-//     arr: [1, 2],
-// })
-
-
-//     arr: [1, 2],
-// })
+reduceCurry((acc, [k, v]) => (acc += v))({ a: 1, b: 2, c: 3 }, 0)
 
 
 
 
+);
 
 
+ function reduceScore(personnel) {
+  const res = filterCurry(([key, val]) => val.isForceUser)(personnel);
+  console.log(res);
+  
+
+  return reduceCurry((acc, [key, val]) => acc + (val.pilotingScore + val.shootingScore)  )(res, 0);
+}
+
+
+   function filterForce(personnel) {
+
+// // prettier-ignore
+// const personnel = {
+//   lukeSkywalker: { id: 5,  pilotingScore: 98, shootingScore: 56, isForceUser: true  },
+//   sabineWren:    { id: 82, pilotingScore: 73, shootingScore: 99, isForceUser: false },
+//   zebOrellios:   { id: 22, pilotingScore: 20, shootingScore: 59, isForceUser: false },
+//   ezraBridger:   { id: 15, pilotingScore: 43, shootingScore: 67, isForceUser: true  },
+//   calebDume:     { id: 11, pilotingScore: 71, shootingScore: 85, isForceUser: true  },
+// }
+
+// console.log(
+
+//     reduceScore(personnel)
+// )
+
+// console.log(filterForce(personnel))
+// console.log(mapAverage(personnel))
+
+
+  return filterCurry(([_, val]) => val.isForceUser == true&& val.shootingScore >= 80)(personnel);
+}
+
+function mapAverage(personnel) {
+    const res = reduceCurry((acc, [key, val]) => acc + (val.pilotingScore + val.shootingScore)) (personnel,0)
+    return res/5
+}
+
+    
+
+    
+
+
+
+
+
+
+
+// // prettier-ignore
+// const personnel = {
+//   lukeSkywalker: { id: 5,  pilotingScore: 98, shootingScore: 56, isForceUser: true  },
+//   sabineWren:    { id: 82, pilotingScore: 73, shootingScore: 99, isForceUser: false },
+//   zebOrellios:   { id: 22, pilotingScore: 20, shootingScore: 59, isForceUser: false },
+//   ezraBridger:   { id: 15, pilotingScore: 43, shootingScore: 67, isForceUser: true  },
+//   calebDume:     { id: 11, pilotingScore: 71, shootingScore: 85, isForceUser: true  },
+// }
+
+// console.log(
+
+//     reduceScore(personnel)
+// )
+
+// console.log(filterForce(personnel))
+// console.log(mapAverage(personnel))
 
