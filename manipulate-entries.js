cd7e0defb47e04dafd615398f362e72a1097d127
mndoumghar <mohmamedndoumghar@gmail.com>
2025-07-04 03:48:02 +0100

    manipulate entries

---
+++ manipulate-entries.js
@@ -0,0 +1,42 @@
+
+function filterEntries(obj,ff) {
+
+    const entries = Object.entries(obj)
+    const filterredEntries = entries.filter(([key, value]) =>ff(key, value)    
+)
+
+    return filterredEntries
+}
+function mapEntries(obj, ff) {
+    const entries = Object.entries(obj)
+    const mapped = entries.map(([key, value]) => ff(key, value))
+    return Object.fromEntries(mapped)
+}
+
+function reduceEntries(obj, ff, i) {
+  const entries = Object.entries(obj)
+  return entries.reduce(
+    (accumulator, [key, value]) => ff(accumulator, key, value),
+    i
+  )
+}
+
+function totalCalories(cart) {
+
+    return reduceEntries(cart, (acc, key, value) => acc + value.calories,0)
+
+
+    
+}
+function lowCarbs(cart) {
+    return filterEntries(cart,(key, value ) => value.carbs<=50)
+
+}
+
+function cartTotal(cart) {
+    
+    return mapEntries(cart, (key,value) => {
+        const total = Object.values(value).reduce((acc, num)=> acc+num, 0)
+        return [key,{total}]
+    })
+}
