d7e747cb43c4b2a2d2de0e03f4c3998c43c9d7b5
mndoumghar <mohmamedndoumghar@gmail.com>
2025-07-05 19:08:07 +0100

    replica

--- replica.js
+++ replica.js
@@ -2,25 +2,37 @@ function replica(output, ...sources) {
   for (const source of sources) {
     for (const key in source) {
       if (typeof source[key] === 'object' && source[key] !== null && !(source[key] instanceof RegExp)) {
-        if (Array.isArray(source[key]) && (!output[key] || !Array.isArray(output[key]))) {
-          output[key] = [];
+
+        if (typeof output[key] !== 'object' || output[key] === null || output[key] instanceof RegExp) {
+        
+          output[key] = Array.isArray(source[key]) ? [] : {};
+        
         }
-        else if (!Array.isArray(source[key]) && (!output[key] || Array.isArray(output[key]))) {
-          output[key] = {};
+        
+        else if (Array.isArray(source[key]) !== Array.isArray(output[key])) {
+        
+          output[key] = Array.isArray(source[key]) ? [] : {};
+        
         }
+        
         replica(output[key], source[key]);
+      
       } else {
         output[key] = source[key];
+      
+     
       }
+    
     }
+  
   }
+  
   return output;
 }
-
 console.log(
 
 
-  (replica({ a: { b: [2] } }, { a: [4] }).a, [4])
+  (replica({ a: 4 }, { a: { b: 1 } })) //, 1)
 
 )
 
