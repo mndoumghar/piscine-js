9e575d7a94153621b5f387bf907b373b0b4ece4d
mndoumghar <mohmamedndoumghar@gmail.com>
2025-07-05 09:37:58 +0100

    deep copy

--- deep-copy.js
+++ deep-copy.js
@@ -1,21 +1,37 @@
-
 function deepCopy(obj) {
-    if (typeof obj !== "object") {
-        return obj
-    }
-    let arr = []
-    if (Array.isArray(obj)) {
-        for (let i = 0; i < obj.length; i++) {
-            arr[i] = deepCopy(obj[i])
-        }
-        return arr
-    }
+  if (typeof obj !== "object" || obj === null) {
+    return obj;
+  }
 
-    const copy = {};
+  if (obj instanceof RegExp) {
+    return new RegExp(obj)
+  }
+
+  if (typeof obj === "function") {
+    return obj
+  }
+
+  if (Array.isArray(obj)) {
+    let arr = [];
+    for (let i = 0; i < obj.length; i++) {
+      arr[i] = deepCopy(obj[i]);
+    }
+    return arr
+  }
+
+  const copy = {};
   for (const key in obj) {
     if (Object.prototype.hasOwnProperty.call(obj, key)) {
-      copy[key] = deepCopy(obj[key])
+      copy[key] = deepCopy(obj[key]);
     }
   }
   return copy;
 }
+
+
+console.log(
+
+deepCopy(/hello/)
+
+
+);
