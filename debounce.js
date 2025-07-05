65558628d5a9abc3c37743085bb35c751ddbe71e
mndoumghar <mohmamedndoumghar@gmail.com>
2025-07-05 11:17:03 +0100

    Asynce -1 depounce

--- debounce.js
+++ debounce.js
@@ -1,11 +1,12 @@
- function debounce(f, wait, options) {
-    let res
-    return function(...arg) {
-       res = setTimeout(f(...arg), wait)
-            return res
-    }
-}
 
 function opDebounce(options) {
     return options
+}
+
+function debounce(f, wait, options) {
+    let res
+    return function(...arg) {
+        res = setTimeout(() => f(...arg), wait)
+        return res
+    }
 }
