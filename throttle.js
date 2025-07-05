fcc1855b97c267419ab8c4a5ec853ddded121fdf
mndoumghar <mohmamedndoumghar@gmail.com>
2025-07-05 16:55:42 +0100

    throttle

--- throttle.js
+++ throttle.js
@@ -1,40 +1,54 @@
 function throttle(f, wait, options = {}) {
+  let lastCallTime = 0;
   let timer = null;
   let lastArgs = null;
-  let shouldCall = !(options.leading);
+
+  const leading = options.leading !== false;  // true by default
+  const trailing = options.trailing !== false; // true by default
+
+  function invoke(args) {
+    lastCallTime = Date.now();
+    f(...args);
+  }
 
   return function (...args) {
-    if (timer) {
-      if (options.trailing) lastArgs = args;
-      return;
+    const now = Date.now();
+    if (!lastCallTime && !leading) {
+      lastCallTime = now;
     }
 
-    if (!shouldCall) {
-      shouldCall = true;
+    const remaining = wait - (now - lastCallTime);
+    lastArgs = args;
+
+    if (remaining <= 0 || remaining > wait) {
+      if (timer) {
+        clearTimeout(timer);
+        timer = null;
+      }
+      invoke(args);
+    } else if (!timer && trailing) {
       timer = setTimeout(() => {
         timer = null;
-        if (options.trailing && lastArgs) {
-          f(...lastArgs);
+        if (trailing && lastArgs) {
+          invoke(lastArgs);
           lastArgs = null;
         }
-      }, wait);
-      return;
+      }, remaining);
     }
-
-    f(...args);
-
-    timer = setTimeout(() => {
-      timer = null;
-      if (options.trailing && lastArgs) {
-        f(...lastArgs);
-        lastArgs = null;
-      }
-    }, wait);
   };
 }
 
+function opThrottle(f, wait, options) {
+  return throttle(f, wait, options);
+}
 
 function opThrottle(f, wait,options) {
     
     return throttle(f, wait,options)
 }
+
+console.log(
+(opThrottle(add, 26, { trailing: true }))    //, 16, 4), 1)
+
+
+)
