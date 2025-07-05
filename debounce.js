1b49a7161c3e9927c6dc7310faa551d9d5d705e8
mndoumghar <mohmamedndoumghar@gmail.com>
2025-07-05 11:07:14 +0100

    Asynce -1 depounce

---
+++ debounce.js
@@ -0,0 +1,9 @@
+ function debounce(f, wait, options) {
+    return function(...arg) {
+            return setTimeout(f(res), wait)
+    }
+}
+
+function opDebounce(options) {
+    return options
+}
--- replica.js
+++ replica.js
@@ -1,7 +1,50 @@
 
+function deepCopy(obj) {
+  if (typeof obj !== "object" || obj === null) {
+    return obj;
+  }
 
-function replica(assign,obj,obj2) {
+  if (obj instanceof RegExp) {
     
-        return Object.assign(assign, obj,obj2)
+    return new RegExp(obj)
+  }
+
+  if (typeof obj === "function") {
+    return obj
+  }
+
+  if (Array.isArray(obj)) {
+    let arr = [];
+    for (let i = 0; i < obj.length; i++) {
+      arr[i] = deepCopy(obj[i]);
+    }
+    return arr
+  }
+
+  const copy = {};
+  for (const key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      copy[key] = deepCopy(obj[key]);
+    }
+  }
+  return copy;
 }
 
+
+
+
+function replica(assign,obj) {
+
+
+  const ss = structuredClone(assign,obj)
+
+        return (deepCopy(ss),deepCopy(obj))  
+}
+
+
+console.log(
+
+
+replica({ a: { b: 1, c: 2 } }, { a: { c: 23 } }) // { a: { b: 1, c: 23 } }
+
+)
