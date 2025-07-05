1233fad071c66935bc14d78ed57d3f6d8abc3c01
mndoumghar <mohmamedndoumghar@gmail.com>
2025-07-05 18:56:37 +0100

    replica

--- replica.js
+++ replica.js
@@ -1,24 +1,13 @@
 function replica(output, ...sources) {
-
-if (output instanceof RegExp) {
-    return {}
-}
-
-
   for (const source of sources) {
-
     for (const key in source) {
-      
-      if (typeof source[key] === 'object' && source[key] !== null ) {
-
+      if (typeof source[key] === 'object' && source[key] !== null && !(source[key] instanceof RegExp)) {
+        
         if (!output[key] || typeof output[key] !== 'object' || output[key] instanceof RegExp) {
-
           output[key] = {};
         }
         replica(output[key], source[key]);
-
       } else {
-
         output[key] = source[key];
       }
     }
@@ -26,11 +15,10 @@ if (output instanceof RegExp) {
   return output;
 }
 
-
 console.log(
 
 
-  replica({ con: console.log }, { reg: /hello/ })
+  replica({ reg: /hello/ })
 
 )
 
