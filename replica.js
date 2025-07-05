16834325a6e61d37c32fd70cd83aab737325a41c
mndoumghar <mohmamedndoumghar@gmail.com>
2025-07-05 18:58:42 +0100

    replica

--- replica.js
+++ replica.js
@@ -2,9 +2,8 @@ function replica(output, ...sources) {
   for (const source of sources) {
     for (const key in source) {
       if (typeof source[key] === 'object' && source[key] !== null && !(source[key] instanceof RegExp)) {
-        
         if (!output[key] || typeof output[key] !== 'object' || output[key] instanceof RegExp) {
-          output[key] = {};
+          output[key] = Array.isArray(source[key]) ? [] : {};
         }
         replica(output[key], source[key]);
       } else {
