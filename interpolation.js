6a03c6f19db63cd046c3ab1d5e82df9ae6c783ec
mndoumghar <mohamedndoumghar@gmail.com>
2025-07-06 23:09:12 +0100

    interpolation

--- interpolation.js
+++ interpolation.js
@@ -1,18 +1,18 @@
 function interpolation(step, start, end, callback, duration) {
-    const stepDuration = duration / step;
+    let current = start;
+    const stepTime = duration / step;
     const increment = (end - start) / step;
-    let currentValue = start;
-    let currentStep = 0;
+    let stepsCompleted = 0;
 
-    const interval = setInterval(() => {
-        currentValue += increment;
-        currentStep++;
+    const intervalId = setInterval(() => {
+        current += increment;
+        stepsCompleted++;
 
-        if (currentStep >= step) {
-            clearInterval(interval);
-            const y = currentValue * 0.2;
-            const x = currentStep * stepDuration + 2;
+        if (stepsCompleted >= step) {
+            clearInterval(intervalId);
+            const y = current * 0.2;
+            const x = stepsCompleted * stepTime + 2;
             callback([y, x]);
         }
-    }, stepDuration * 1000);
+    }, stepTime * 1000);
 }
