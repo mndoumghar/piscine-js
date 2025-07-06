18a346caf2dae3999b508b817c1df5c0483fd9df
mndoumghar <mohamedndoumghar@gmail.com>
2025-07-06 18:20:39 +0100

    throttle

--- throttle.js
+++ throttle.js
@@ -1,21 +1,22 @@
-
 function throttle(func, wait, options) {
-  var context, args, result;
-  var timeout = null;
+  var timeout, context, args, result;
   var previous = 0;
   if (!options) options = {};
-  var later = function () {
+
+  var later = function() {
     previous = options.leading === false ? 0 : Date.now();
     timeout = null;
     result = func.apply(context, args);
     if (!timeout) context = args = null;
   };
-  return function () {
+
+  var throttled = function() {
     var now = Date.now();
     if (!previous && options.leading === false) previous = now;
     var remaining = wait - (now - previous);
     context = this;
     args = arguments;
+    
     if (remaining <= 0 || remaining > wait) {
       if (timeout) {
         clearTimeout(timeout);
@@ -29,14 +30,16 @@ function throttle(func, wait, options) {
     }
     return result;
   };
-};
 
+  throttled.cancel = function() {
+    clearTimeout(timeout);
+    previous = 0;
+    timeout = context = args = null;
+  };
 
-
-
-
+  return throttled;
+}
 
 function opThrottle(f, wait, options) {
-
   return throttle(f, wait, options);
 }
