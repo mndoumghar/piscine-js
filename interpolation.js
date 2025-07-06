e58bd36a9d2adb5b99f220ba3de2b3aef70523af
mndoumghar <mohamedndoumghar@gmail.com>
2025-07-06 22:56:35 +0100

    interpolation

--- interpolation.js
+++ interpolation.js
@@ -1,24 +1,36 @@
-
 function interpolation(step, start, end, callback, duration) {
-    
     let evrystep = duration / step
     let z = start
-    let conter = 0
-    const increment = (end - start)
-
+        let conter = 0
+        const increment = (end - start) 
     let res = setInterval(() => {
-        let y = z * 0.2
-        let x = conter * evrystep + 2
-        z += increment
-        conter += evrystep
-        if (z >= step) {
+        let y = z* 0.2
+        let x = conter * evrystep+2
+
+       callback([y, x])
+
+        z+= increment
+
+        conter +=  evrystep
+
+
+        if ( z== step || end == y) {
             clearInterval(res)
             callback([y, x])
-  
         }
-  
-        conter = z
+        conter= z
+
 
     }, 1000 * evrystep)
 
 }
+
+interpolation(
+    5,
+    0,
+    1,
+    (([x, y]) => {
+        console.log([x, y])
+    })
+    ,
+    10)
